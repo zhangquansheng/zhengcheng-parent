78475c7c8fa70c10bfbcb10872f62b15dcc5ad00
zqs <952547584@qq.com>
2019-03-21 18:21:22 +0800

    mvc增加开发脚手架

---
+++ mvc-spring-boot-starter/src/main/java/com/zhengcheng/mvc/controller/BaseController.java
@@ -0,0 +1,13 @@
+package com.zhengcheng.mvc.controller;
+
+/**
+ * BaseController
+ *
+ * @author :    quansheng.zhang
+ * @Package :     com.zhengcheng.mvc.controller
+ * @Description :
+ * @date :    2019/3/21 18:08
+ */
+public class BaseController {
+
+}
