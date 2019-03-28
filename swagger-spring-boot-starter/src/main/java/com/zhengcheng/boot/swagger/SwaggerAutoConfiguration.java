1723f4db4d38af68dfb491602a0502fd3cafaa0a
zqs <952547584@qq.com>
2019-03-28 15:26:26 +0800

    swagger

--- swagger-spring-boot-starter/src/main/java/com/zhengcheng/boot/swagger/SwaggerAutoConfiguration.java
+++ swagger-spring-boot-starter/src/main/java/com/zhengcheng/boot/swagger/SwaggerAutoConfiguration.java
@@ -61,8 +61,12 @@ public class SwaggerAutoConfiguration {
                 .build();
     }
 
+    private ApiKey apiKey() {
+        return new ApiKey("Bearer", "Authorization", "header");
+    }
+
     private List<ApiKey> securitySchemes() {
-        return new ArrayList(Collections.singleton(new ApiKey("Authorization", "Authorization", "header")));
+        return new ArrayList(Collections.singleton(new ApiKey("Bearer", "Authorization", "header")));
     }
 
     private List<SecurityContext> securityContexts() {
@@ -78,6 +82,6 @@ public class SwaggerAutoConfiguration {
         AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
         AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
         authorizationScopes[0] = authorizationScope;
-        return new ArrayList(Collections.singleton(new SecurityReference("Authorization", authorizationScopes)));
+        return new ArrayList(Collections.singleton(new SecurityReference("Bearer", authorizationScopes)));
     }
 }
