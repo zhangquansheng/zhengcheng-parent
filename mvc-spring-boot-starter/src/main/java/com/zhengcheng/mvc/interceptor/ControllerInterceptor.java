f2530257c33922be1accfa225c1a13b6c2ba53df
zqs <952547584@qq.com>
2019-06-27 11:20:52 +0800

    日志，使用logback sl4j

--- mvc-spring-boot-starter/src/main/java/com/zhengcheng/mvc/interceptor/ControllerInterceptor.java
+++ mvc-spring-boot-starter/src/main/java/com/zhengcheng/mvc/interceptor/ControllerInterceptor.java
@@ -4,13 +4,19 @@ import com.alibaba.fastjson.JSON;
 import com.zhengcheng.common.exception.BizException;
 import com.zhengcheng.common.web.Result;
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
-import org.aspectj.lang.reflect.MethodSignature;
 import org.nutz.lang.Lang;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.Serializable;
+import java.util.Map;
 
 
 /**
@@ -45,28 +51,50 @@ public class ControllerInterceptor {
     @Around("controllerMethodPointcut()")
     public Object interceptor(ProceedingJoinPoint pjp) {
         long beginTime = System.currentTimeMillis();
-        MethodSignature signature = (MethodSignature) pjp.getSignature();
-        String className = signature.getDeclaringTypeName();
-        String methodName = signature.getName();
-        Object[] args = pjp.getArgs();
-        String argsJsonStr = "";
-        if (args != null) {
-            argsJsonStr = JSON.toJSONString(args);
-        }
         Object retObj;
         try {
             retObj = pjp.proceed();
         } catch (Throwable e) {
             if (e instanceof BizException) {
-                log.info("BizException#{}#{}#{}请求异常，错误：{}", className, methodName, argsJsonStr, e.getMessage());
                 retObj = Result.errorMessage(e.getMessage());
+                log.info("{}请求异常，错误：{}", this.getMethodInfo(pjp), e.getMessage());
             } else {
-                log.error("{}#{}#{}请求异常，错误：{}", className, methodName, argsJsonStr, e.getMessage(), e);
                 retObj = Result.errorData("服务异常，请联系技术人员", Lang.getStackTrace(e));
+                log.error("{}请求异常，错误：{}", this.getMethodInfo(pjp), e.getMessage(), e);
             }
         }
         long costMs = System.currentTimeMillis() - beginTime;
-        log.info("{}#{}#{}请求结束，耗时：{}ms", className, methodName, argsJsonStr, costMs);
+        if (retObj instanceof Result) {
+            log.info("{}请求结束，耗时：{}ms", this.getMethodInfo(pjp), costMs);
+        }
         return retObj;
+    }
+
+    private String getMethodInfo(JoinPoint point) {
+        StringBuilder sb = new StringBuilder();
+        try {
+            HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+            String method = request.getMethod();
+            String className = point.getSignature().getDeclaringType().getSimpleName();
+            String methodName = point.getSignature().getName();
+            sb.append(className).append(".").append(methodName);
+            if ("POST".equalsIgnoreCase(method)) {
+                Object[] args = point.getArgs();
+                if (args.length > 0) {
+                    if (args[0] instanceof Serializable) {
+                        sb.append(" | args:").append(JSON.toJSONString(args[0]));
+                    }
+                }
+                Map<String, String[]> parameterMap = request.getParameterMap();
+                if (parameterMap != null && parameterMap.size() > 0) {
+                    sb.append(" | param:").append(JSON.toJSONString(parameterMap));
+                }
+            } else {
+                sb.append(" | param:").append(request.getQueryString());
+            }
+        } catch (Exception e) {
+            sb.append("|Exception:").append(e.getMessage());
+        }
+        return sb.toString();
     }
 }
