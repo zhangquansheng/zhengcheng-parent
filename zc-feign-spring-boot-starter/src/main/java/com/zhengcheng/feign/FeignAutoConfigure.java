fc273c495ad083bdb25b71db5f35973a56f63c50
zqs <952547584@qq.com>
2019-09-15 11:10:32 +0800

    FeignAutoConfigure

---
+++ zc-feign-spring-boot-starter/src/main/java/com/zhengcheng/feign/FeignAutoConfigure.java
@@ -0,0 +1,24 @@
+package com.zhengcheng.feign;
+
+import com.zhengcheng.feign.config.FeignOkHttpConfig;
+import feign.Logger;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+
+/**
+ * Feign统一配置
+ *
+ * @author :    quansheng.zhang
+ * @date :    2019/7/28 21:31
+ */
+@Import({FeignOkHttpConfig.class})
+public class FeignAutoConfigure {
+
+    /**
+     * Feign 日志级别
+     */
+    @Bean
+    Logger.Level feignLoggerLevel() {
+        return Logger.Level.FULL;
+    }
+}
---
+++ zc-feign-spring-boot-starter/src/main/java/com/zhengcheng/feign/config/FeignOkHttpConfig.java
@@ -0,0 +1,41 @@
+package com.zhengcheng.feign.config;
+
+import feign.Feign;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.cloud.openfeign.FeignAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * FeignOkHttpConfig
+ *
+ * @author :    quansheng.zhang
+ * @date :    2019/6/29 16:07
+ */
+@Configuration
+@ConditionalOnClass(Feign.class)
+@AutoConfigureBefore(FeignAutoConfiguration.class)
+public class FeignOkHttpConfig {
+
+    @Value("${feign.okhttp3.read-timeout.milliseconds:500}")
+    private Long readTimeout;
+    @Value("${feign.okhttp3.connect-timeout.milliseconds:1000}")
+    private Long connectTimeout;
+    @Value("${feign.okhttp3.write-timeout.milliseconds:60000}")
+    private Long writeTimeout;
+
+    @Bean
+    public okhttp3.OkHttpClient okHttpClient() {
+        return new okhttp3.OkHttpClient.Builder()
+                .readTimeout(readTimeout, TimeUnit.MILLISECONDS)
+                .connectTimeout(connectTimeout, TimeUnit.MILLISECONDS)
+                .writeTimeout(writeTimeout, TimeUnit.MILLISECONDS)
+                .retryOnConnectionFailure(false)
+                .connectionPool(new okhttp3.ConnectionPool())
+                .build();
+    }
+}
