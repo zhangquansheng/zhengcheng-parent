5725d4a9c88d2c72c6df704c6eac5c7258bd5dec
zqs <952547584@qq.com>
2019-09-30 18:12:04 +0800

    Security 权限常量

--- zc-common-spring-boot-starter/src/main/java/com/zhengcheng/common/constant/SecurityConstants.java
+++ zc-common-spring-boot-starter/src/main/java/com/zhengcheng/common/constant/SecurityConstants.java
@@ -29,11 +29,6 @@ public interface SecurityConstants {
     String CLIENT_HEADER = "x-client-header";
 
     /**
-     * 基础角色
-     */
-    String BASE_ROLE = "ROLE_USER";
-
-    /**
      * 授权码模式
      */
     String AUTHORIZATION_CODE = "authorization_code";
@@ -49,122 +44,11 @@ public interface SecurityConstants {
     String REFRESH_TOKEN = "refresh_token";
 
     /**
-     * oauth token
-     */
-    String OAUTH_TOKEN_URL = "/oauth/token";
-
-    /**
-     * 默认的处理验证码的url前缀
-     */
-    String DEFAULT_VALIDATE_CODE_URL_PREFIX = "/validata/code";
-
-    /**
-     * 手机号的处理验证码的url前缀
-     */
-    String MOBILE_VALIDATE_CODE_URL_PREFIX = "/validata/smsCode";
-
-    /**
-     * 默认生成图形验证码宽度
-     */
-    String DEFAULT_IMAGE_WIDTH = "100";
-
-    /**
-     * 默认生成图像验证码高度
-     */
-    String DEFAULT_IMAGE_HEIGHT = "35";
-
-    /**
-     * 默认生成图形验证码长度
-     */
-    String DEFAULT_IMAGE_LENGTH = "4";
-
-    /**
-     * 默认生成图形验证码过期时间
-     */
-    int DEFAULT_IMAGE_EXPIRE = 60;
-
-    /**
-     * 边框颜色，合法值： r,g,b (and optional alpha) 或者 white,black,blue.
-     */
-    String DEFAULT_COLOR_FONT = "blue";
-
-    /**
-     * 图片边框
-     */
-    String DEFAULT_IMAGE_BORDER = "no";
-
-    /**
-     * 默认图片间隔
-     */
-    String DEFAULT_CHAR_SPACE = "5";
-
-    /**
-     * 默认保存code的前缀
-     */
-    String DEFAULT_CODE_KEY = "DEFAULT_CODE_KEY";
-
-    /**
-     * 验证码文字大小
-     */
-    String DEFAULT_IMAGE_FONT_SIZE = "30";
-    /**
-     * zlt公共前缀
-     */
-    String ZLT_PREFIX = "zlt:";
-    /**
-     * 缓存client的redis key，这里是hash结构存储
-     */
-    String CACHE_CLIENT_KEY = "oauth_client_details";
-    /**
-     * OAUTH模式登录处理地址
-     */
-    String OAUTH_LOGIN_PRO_URL = "/user/login";
-    /**
-     * PASSWORD模式登录处理地址
-     */
-    String PASSWORD_LOGIN_PRO_URL = "/oauth/user/token";
-    /**
-     * 获取授权码地址
-     */
-    String AUTH_CODE_URL = "/oauth/authorize";
-    /**
-     * 登录页面
-     */
-    String LOGIN_PAGE = "/login.html";
-    /**
-     * 默认的OPENID登录请求处理url
-     */
-    String OPENID_TOKEN_URL = "/oauth/openId/token";
-    /**
-     * 手机登录URL
-     */
-    String MOBILE_TOKEN_URL = "/oauth/mobile/token";
-    /**
-     * 登出URL
-     */
-    String LOGOUT_URL = "/oauth/remove/token";
-    /**
-     * 默认token过期时间(1小时)
-     */
-    Integer ACCESS_TOKEN_VALIDITY_SECONDS = 60 * 60;
-    /**
-     * redis中授权token对应的key
-     */
-    String REDIS_TOKEN_AUTH = "auth:";
-    /**
-     * redis中应用对应的token集合的key
-     */
-    String REDIS_CLIENT_ID_TO_ACCESS = "client_id_to_access:";
-    /**
-     * redis中用户名对应的token集合的key
-     */
-    String REDIS_UNAME_TO_ACCESS = "uname_to_access:";
-    /**
-     * rsa公钥
-     */
-    String RSA_PUBLIC_KEY = "pubkey.txt";
-    /**
      * 登录失败提示
      */
     String LOGIN_FAIL_MESSAGE = "用户名不存在或者密码错误";
+    /**
+     * 授权登录失败提示
+     */
+    String AUTH_FAIL_MESSAGE = "授权信息错误";
 }
