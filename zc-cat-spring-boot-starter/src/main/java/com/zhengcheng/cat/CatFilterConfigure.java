1c1d3602a53eb6c3bf20b1983007e5520c8c2308
zqs <952547584@qq.com>
2019-12-04 23:31:11 +0800

    CAT通用组件 spring-boot 集成

---
+++ zc-cat-spring-boot-starter/src/main/java/com/zhengcheng/cat/CatFilterConfigure.java
@@ -0,0 +1,27 @@
+package com.zhengcheng.cat;
+
+import com.dianping.cat.servlet.CatFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+/**
+ * CatFilterConfigure
+ *
+ * @author :    quansheng.zhang
+ * @date :    2019/12/4 23:27
+ */
+@Configuration
+public class CatFilterConfigure {
+
+    @Bean
+    public FilterRegistrationBean catFilter() {
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        CatFilter filter = new CatFilter();
+        registration.setFilter(filter);
+        registration.addUrlPatterns("/*");
+        registration.setName("cat-filter");
+        registration.setOrder(1);
+        return registration;
+    }
+
+}
