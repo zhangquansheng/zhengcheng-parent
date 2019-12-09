3cd435372699800a0281b176d5ce461c6247fca6
zqs <952547584@qq.com>
2019-12-09 21:59:40 +0800

    FeignPluginTemplate

---
+++ zc-cat-spring-boot-starter/src/main/java/com/zhengcheng/cat/plugin/feign/FeignPluginTemplate.java
@@ -0,0 +1,43 @@
+package com.zhengcheng.cat.plugin.feign;
+
+import cn.hutool.core.text.StrBuilder;
+import com.dianping.cat.Cat;
+import com.dianping.cat.message.Transaction;
+import com.zhengcheng.cat.plugin.AbstractPluginTemplate;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+
+/**
+ * FeignPluginTemplate
+ *
+ * @author :    quansheng.zhang
+ * @date :    2019/12/9 21:51
+ */
+@Aspect
+public class FeignPluginTemplate extends AbstractPluginTemplate {
+
+    @Pointcut("execution(* org.springframework.cloud.openfeign.ribbon.LoadBalancerFeignClient.*(..))")
+    public void feignClientPointcut() {
+    }
+
+    @Override
+    @Around("feignClientPointcut()")
+    public Object doAround(ProceedingJoinPoint pjp) throws Throwable {
+        return super.doAround(pjp);
+    }
+
+    @Override
+    protected Transaction beginLog(ProceedingJoinPoint pjp) {
+        StrBuilder sb = StrBuilder.create();
+        String className = pjp.getSignature().getDeclaringType().getSimpleName();
+        String methodName = pjp.getSignature().getName();
+        return Cat.newTransaction("RemoteCall", sb.append(className).append(".").append(methodName).toString());
+    }
+
+    @Override
+    protected void endLog(Transaction transaction, Object retVal, Object... params) {
+
+    }
+}
