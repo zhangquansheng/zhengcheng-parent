c9a04129869cb904bef09b3080d71292aa03bbe7
zqs <952547584@qq.com>
2019-12-18 21:31:41 +0800

    NettySocket自动配置

---
+++ zc-netty-socketio-spring-boot-starter/src/main/java/com/zhengcheng/socketio/NettySocketAutoConfiguration.java
@@ -0,0 +1,14 @@
+package com.zhengcheng.socketio;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * NettySocket自动配置
+ *
+ * @author :    quansheng.zhang
+ * @date :    2019/12/18 21:31
+ */
+@Configuration
+public class NettySocketAutoConfiguration {
+
+}
