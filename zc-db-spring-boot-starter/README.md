f692041b78a229f9197083aa297d8d8f1fd87cdf
zqs <952547584@qq.com>
2019-12-22 16:33:18 +0800

    Mysql数据库通用组件

---
+++ zc-db-spring-boot-starter/README.md
@@ -0,0 +1,13 @@
+# Mysql数据库通用组件
+
+## **简介**（Introduction）
+
+数据库基于Mysql5.6以上，使用[MybatisPlus](https://mp.baomidou.com/)
+
+## **入门篇**
+
+### **环境准备**
+
+### **安装**
+
+### **设置**
---
+++ zc-db-spring-boot-starter/src/main/java/com/zhengcheng/db/MybatisPlusAutoConfiguration.java
@@ -0,0 +1,15 @@
+package com.zhengcheng.db;
+
+import com.zhengcheng.db.config.DefaultMybatisPlusConfig;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * mybatis-plus配置
+ *
+ * @author :    quansheng.zhang
+ * @date :    2019/7/28 21:31
+ */
+@Configuration
+public class MybatisPlusAutoConfiguration extends DefaultMybatisPlusConfig {
+
+}
