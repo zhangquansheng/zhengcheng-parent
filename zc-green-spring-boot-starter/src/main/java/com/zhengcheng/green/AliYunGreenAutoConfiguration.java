b701f0515695160d44dadf928dcd2bacb149759a
zhangquansheng <952547584@qq>
2020-01-16 18:17:43 +0800

    腾讯云-内容安全

--- zc-green-spring-boot-starter/src/main/java/com/zhengcheng/green/AliYunGreenAutoConfiguration.java
+++ zc-green-spring-boot-starter/src/main/java/com/zhengcheng/green/AliYunGreenAutoConfiguration.java
@@ -4,19 +4,24 @@ import com.zhengcheng.green.properties.AcsProperties;
 import com.zhengcheng.green.service.IAliYunGreenService;
 import com.zhengcheng.green.service.impl.AliYunGreenServiceImpl;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
- * 内容安全
+ * 阿里云内容安全
  *
  * @author :    quansheng.zhang
  * @date :    2019/12/2 23:01
  */
 @Configuration
 @EnableConfigurationProperties({AcsProperties.class})
-public class GreenAutoConfiguration {
+@ConditionalOnProperty(
+        prefix = "aliyun.acs",
+        name = "access-key-id"
+)
+public class AliYunGreenAutoConfiguration {
 
     @Bean
     @ConditionalOnMissingBean
---
+++ zc-green-spring-boot-starter/src/main/java/com/zhengcheng/green/TencentcloudAutoConfiguration.java
@@ -0,0 +1,32 @@
+package com.zhengcheng.green;
+
+import com.zhengcheng.green.properties.TencentcloudProperties;
+import com.zhengcheng.green.service.ITencentcloudCmsService;
+import com.zhengcheng.green.service.impl.TencentcloudCmsServiceImpl;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 腾讯云内容安全
+ *
+ * @author :    zhangquansheng
+ * @date :    2020/1/16 17:54
+ */
+@Configuration
+@EnableConfigurationProperties({TencentcloudProperties.class})
+@ConditionalOnProperty(
+        prefix = "tencentcloud",
+        name = "secret-id"
+)
+public class TencentcloudAutoConfiguration {
+
+    @Bean
+    @ConditionalOnMissingBean
+    public ITencentcloudCmsService tencentcloudCmsService(TencentcloudProperties tencentcloudProperties) {
+        return new TencentcloudCmsServiceImpl(tencentcloudProperties);
+    }
+
+}
