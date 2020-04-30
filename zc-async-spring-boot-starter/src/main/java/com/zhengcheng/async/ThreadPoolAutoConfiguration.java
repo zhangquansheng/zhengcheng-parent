94e7490c63483b628e848c51eebb213efce3500a
zhangquansheng <952547584@qq.com>
2020-04-30 10:08:48 +0800

    异步线程池

--- zc-async-spring-boot-starter/src/main/java/com/zhengcheng/async/ThreadPoolAutoConfiguration.java
+++ zc-async-spring-boot-starter/src/main/java/com/zhengcheng/async/ThreadPoolAutoConfiguration.java
@@ -3,7 +3,6 @@ package com.zhengcheng.async;
 import com.zhengcheng.async.decorator.MdcTaskDecorator;
 import com.zhengcheng.async.executor.VisibleThreadPoolTaskExecutor;
 import com.zhengcheng.async.properties.ExecutorProperties;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,13 +18,16 @@ import java.util.concurrent.ThreadPoolExecutor;
  * @author :    quansheng.zhang
  * @date :    2020/3/15 12:47
  */
+@EnableConfigurationProperties({ExecutorProperties.class})
 @EnableAsync
 @Configuration
-@EnableConfigurationProperties(ExecutorProperties.class)
 public class ThreadPoolAutoConfiguration {
 
-    @Autowired
-    private ExecutorProperties executorProperties;
+    private final ExecutorProperties executorProperties;
+
+    public ThreadPoolAutoConfiguration(ExecutorProperties executorProperties) {
+        this.executorProperties = executorProperties;
+    }
 
     @Bean
     public Executor taskExecutor() {
@@ -36,7 +38,6 @@ public class ThreadPoolAutoConfiguration {
         executor.setKeepAliveSeconds(executorProperties.getKeepAliveSeconds());
         executor.setAwaitTerminationSeconds(executorProperties.getAwaitTerminationSeconds());
         executor.setThreadNamePrefix(executorProperties.getThreadNamePrefix());
-        // 添加装饰器，为mdc traceId
         executor.setTaskDecorator(new MdcTaskDecorator());
         executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
         //执行初始化
