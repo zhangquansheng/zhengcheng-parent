0dd1111e8809bd2cf8ea95a7230fa801a5901d68
zhangquansheng <952547584@qq.com>
2020-05-06 17:21:02 +0800

    Curator实现zk分布式锁工具类

--- zc-redis-spring-boot-starter/src/main/java/com/zhengcheng/redis/lock/CuratorDistributedLock.java
+++ zc-redis-spring-boot-starter/src/main/java/com/zhengcheng/redis/lock/CuratorDistributedLock.java
@@ -4,9 +4,6 @@ import com.zhengcheng.common.lock.AbstractDistributedLock;
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.curator.framework.CuratorFramework;
-import org.apache.curator.framework.imps.CuratorFrameworkState;
-import org.apache.curator.framework.recipes.locks.InterProcessLock;
-import org.apache.curator.framework.recipes.locks.InterProcessMutex;
 
 /**
  * Curator实现zk分布式锁工具类
@@ -25,26 +22,28 @@ public class CuratorDistributedLock extends AbstractDistributedLock {
 
     @Override
     public boolean lock(String key, long expire, int retryTimes, long sleepMillis) {
-        if (!CuratorFrameworkState.STARTED.equals(curatorFramework.getState())) {
-            log.warn("instance must be started before calling this method");
-            return false;
-        }
-        String nodePath = "/curator/lock/%s";
-        InterProcessMutex mutex = new InterProcessMutex(curatorFramework, String.format(nodePath, key));
-        boolean locked = mutex.acquire(acquireTimeout, TimeUnit.SECONDS);
-        ZookeeperLockContext.setContext(mutex);
-        return locked;
+//        if (!CuratorFrameworkState.STARTED.equals(curatorFramework.getState())) {
+//            log.warn("instance must be started before calling this method");
+//            return false;
+//        }
+//        String nodePath = "/curator/lock/%s";
+//        InterProcessMutex mutex = new InterProcessMutex(curatorFramework, String.format(nodePath, key));
+//        boolean locked = mutex.acquire(acquireTimeout, TimeUnit.SECONDS);
+//        ZookeeperLockContext.setContext(mutex);
+//        return locked;
+        return false;
     }
 
     @Override
     public boolean releaseLock(String key) {
-        InterProcessLock interProcessLock = ZookeeperLockContext.getContext();
-        if (null != interProcessLock) {
-            try {
-                interProcessLock.release();
-            } catch (Exception e) {
-                log.warn("zookeeper lock release error", e);
-            }
-        }
+//        InterProcessLock interProcessLock = ZookeeperLockContext.getContext();
+//        if (null != interProcessLock) {
+//            try {
+//                interProcessLock.release();
+//            } catch (Exception e) {
+//                log.warn("zookeeper lock release error", e);
+//            }
+//        }
+        return false;
     }
 }
