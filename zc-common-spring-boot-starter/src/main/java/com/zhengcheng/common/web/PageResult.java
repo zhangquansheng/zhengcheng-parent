9be788fae085e70e2058a7d505feaddf60c20b1e
zhangquansheng <952547584@qq.com>
2020-05-06 17:29:03 +0800

    分页结果

--- zc-common-spring-boot-starter/src/main/java/com/zhengcheng/common/web/PageResult.java
+++ zc-common-spring-boot-starter/src/main/java/com/zhengcheng/common/web/PageResult.java
@@ -3,9 +3,9 @@ package com.zhengcheng.common.web;
 import com.zhengcheng.common.constant.CommonConstants;
 import com.zhengcheng.common.enumeration.CodeEnum;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import org.slf4j.MDC;
 
+import java.io.Serializable;
 import java.util.List;
 
 /**
@@ -14,19 +14,24 @@ import java.util.List;
  * @author :    zhangquansheng
  * @date :    2020/4/26 14:33
  */
-@EqualsAndHashCode(callSuper = true)
 @Data
-public class PageResult<T> extends Result<T> {
+public class PageResult<T> implements Serializable {
     private static final long serialVersionUID = 556110794424864630L;
-    /**
-     * 总数
-     */
-    private Long total;
     /**
      * 错误码，和HTTP状态码一致（但有扩展）。
      * 2xx：表示调用成功
      * 4xx：表示请求有误
      * 5xx：表示后端有误
+     */
+    private Integer code;
+    private String message;
+    private String requestId;
+    /**
+     * 总数
+     */
+    private Long total;
+    /**
+     * 数据
      */
     private List<T> data;
 
@@ -43,4 +48,5 @@ public class PageResult<T> extends Result<T> {
     public static <T> PageResult<T> successData(PageInfo<T> pageInfo) {
         return PageResult.successData(pageInfo.getTotal(), pageInfo.getList());
     }
+
 }
