4e0b6e1a9d98705f3e7e6aeeef64b8d6141fa5d4
zhangquansheng <952547584@qq.com>
2020-05-07 15:44:13 +0800

    Web模块自动配置

--- zc-web-spring-boot-starter/src/main/java/com/zhengcheng/web/WebAutoConfiguration.java
+++ zc-web-spring-boot-starter/src/main/java/com/zhengcheng/web/WebAutoConfiguration.java
@@ -2,11 +2,9 @@ package com.zhengcheng.web;
 
 import com.zhengcheng.web.aspect.ControllerLogAspect;
 import com.zhengcheng.web.interceptor.TraceIdInterceptor;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -27,11 +25,6 @@ public class WebAutoConfiguration implements WebMvcConfigurer {
     public WebAutoConfiguration() {
     }
 
-    @Bean
-    public HandlerInterceptor getTraceIdInterceptor() {
-        return new TraceIdInterceptor();
-    }
-
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
@@ -44,7 +37,7 @@ public class WebAutoConfiguration implements WebMvcConfigurer {
     //添加拦截
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(getTraceIdInterceptor())//路径拦截器
+        registry.addInterceptor(new TraceIdInterceptor())//路径拦截器
                 .addPathPatterns("/**")//拦截的请求路径
                 .excludePathPatterns("/error")//排除的请求路径
                 .excludePathPatterns("/static/*");
