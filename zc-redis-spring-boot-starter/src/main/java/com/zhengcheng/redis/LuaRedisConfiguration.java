4e31dfbb7b89b69f6c13114eac39fa4078734603
zqs <952547584@qq.com>
2020-05-12 23:11:33 +0800

    修复当value一致也会导致接口缓存重叠

--- zc-redis-spring-boot-starter/src/main/java/com/zhengcheng/redis/LuaRedisConfiguration.java
+++ zc-redis-spring-boot-starter/src/main/java/com/zhengcheng/redis/LuaRedisConfiguration.java
@@ -1,10 +1,21 @@
 package com.zhengcheng.redis;
 
+import cn.hutool.core.util.StrUtil;
+import com.zhengcheng.redis.annotation.RequestLimit;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.script.DefaultRedisScript;
 import org.springframework.scripting.support.ResourceScriptSource;
+
+import java.lang.reflect.Method;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Lua脚本配置
@@ -12,8 +23,9 @@ import org.springframework.scripting.support.ResourceScriptSource;
  * @author :    zhangquansheng
  * @date :    2020/5/12 16:05
  */
+@Slf4j
 @Configuration
-public class LuaRedisConfiguration {
+public class LuaRedisConfiguration implements ApplicationContextAware {
 
     @Bean
     public DefaultRedisScript<Boolean> redisScript() {
@@ -21,5 +33,30 @@ public class LuaRedisConfiguration {
         redisScript.setScriptSource(new ResourceScriptSource(new ClassPathResource("script/request_limit.lua")));
         redisScript.setResultType(Boolean.class);
         return redisScript;
+    }
+
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        Set<String> requestLimitSet = new HashSet<>();
+        String[] beanDefinitionNames = applicationContext.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            Object bean = applicationContext.getBean(beanDefinitionName);
+            Method[] methods = bean.getClass().getDeclaredMethods();
+            for (Method method : methods) {
+                RequestLimit requestLimit = AnnotationUtils.findAnnotation(method, RequestLimit.class);
+                assert requestLimit != null;
+                String name = requestLimit.name();
+                if (StrUtil.isBlank(name)) {
+                    continue;
+                }
+                if (requestLimitSet.contains(name)) {
+                    throw new RuntimeException("RequestLimit[" + name + "] naming conflicts.");
+                } else {
+                    requestLimitSet.add(name);
+                    log.info("Generating unique request-limit operation named: {}", name);
+                }
+            }
+        }
     }
 }
