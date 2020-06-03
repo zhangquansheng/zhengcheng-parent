a0a6d74eeb2c14c7f2c641951f1bcbf910e290c2
zqs <952547584@qq.com>
2020-06-03 20:42:54 +0800

    统一返回结果处理

---
+++ zc-web-spring-boot-starter/src/main/java/com/zhengcheng/web/advice/GlobalResponseBodyAdvice.java
@@ -0,0 +1,37 @@
+package com.zhengcheng.web.advice;
+
+import com.zhengcheng.common.web.PageResult;
+import com.zhengcheng.common.web.Result;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+/**
+ * 统一返回结果处理
+ *
+ * @author :    quansheng.zhang
+ * @date :    2019/2/28 21:00
+ */
+@RestControllerAdvice
+public class GlobalResponseBodyAdvice implements ResponseBodyAdvice<Object> {
+
+    @Override
+    public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType,
+                                  Class<? extends HttpMessageConverter<?>> selectedConverterType,
+                                  ServerHttpRequest request, ServerHttpResponse response) {
+        if (body instanceof Result || body instanceof PageResult) {
+            return body;
+        }
+        return Result.successData(body);
+    }
+
+    @Override
+    public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
+        // 此处可通过returnType.getDeclaringClass()  returnType.getMethod().getName() 过滤不想拦截的类或者方法。
+        return true;
+    }
+}
