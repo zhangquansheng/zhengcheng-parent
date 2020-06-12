c2923ad0bb20e49900ae82ca9d154f351ada93bf
zqs <952547584@qq.com>
2020-06-13 00:40:24 +0800

    ZK 分布式锁

--- zc-zk-spring-boot-starter/src/main/java/com/zhengcheng/zk/ZkAutoConfiguration.java
+++ zc-zk-spring-boot-starter/src/main/java/com/zhengcheng/zk/ZkAutoConfiguration.java
@@ -1,11 +1,15 @@
 package com.zhengcheng.zk;
 
+import com.zhengcheng.common.lock.DistributedLock;
+import com.zhengcheng.zk.lock.ZkDistributedLock;
 import com.zhengcheng.zk.properties.CuratorProperties;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.curator.RetryPolicy;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.retry.ExponentialBackoffRetry;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,6 +20,7 @@ import org.springframework.context.annotation.Configuration;
  * @author :    zhangquansheng
  * @date :    2020/5/29 14:57
  */
+@Slf4j
 @Configuration
 @EnableConfigurationProperties({CuratorProperties.class})
 public class ZkAutoConfiguration {
@@ -32,4 +37,19 @@ public class ZkAutoConfiguration {
         return CuratorFrameworkFactory.newClient(curatorProperties.getZookeeperConnectionString(), retryPolicy);
     }
 
+    /**
+     * ZK 分布式锁
+     * zk.lock.enable = true
+     *
+     * @param curatorFramework 客户端
+     * @return 分布式锁
+     */
+    @Bean
+    @ConditionalOnProperty(name = "zk.lock.enable", havingValue = "true")
+    public DistributedLock distributedLock(CuratorFramework curatorFramework) {
+        if (log.isDebugEnabled()) {
+            log.debug("ZK 分布式锁配置成功");
+        }
+        return new ZkDistributedLock(curatorFramework);
+    }
 }
