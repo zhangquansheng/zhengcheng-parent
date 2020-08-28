9291c44c7e1ae262415940f668a96ea4b112274d
zhangquansheng <952547584@qq.com>
2020-08-28 08:54:03 +0800

    fix: 控制层日志打印

--- zc-web-spring-boot-starter/src/main/java/com/zhengcheng/web/aspect/ControllerLogAspect.java
+++ zc-web-spring-boot-starter/src/main/java/com/zhengcheng/web/aspect/ControllerLogAspect.java
@@ -1,9 +1,9 @@
 package com.zhengcheng.web.aspect;
 
 import cn.hutool.core.text.StrBuilder;
+import cn.hutool.core.util.StrUtil;
 import cn.hutool.http.Method;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.zhengcheng.common.constant.CommonConstants;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
@@ -12,7 +12,6 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.multipart.MultipartFile;
@@ -29,9 +28,8 @@ import java.util.Objects;
  * @date :    2019/11/28 16:04
  */
 @Slf4j
+@ConditionalOnClass(ObjectMapper.class)
 @Aspect
-@ConditionalOnClass({ObjectMapper.class})
-@Component
 public class ControllerLogAspect {
 
     @Autowired
@@ -48,11 +46,11 @@ public class ControllerLogAspect {
     @Around("controllerMethodPointcut()")
     public Object around(ProceedingJoinPoint pjp) throws Throwable {
         String pjpMethodInfo = this.getMethodInfo(pjp);
-        log.info("{} | {}", pjpMethodInfo, this.getRequestInfo(pjp));
+        log.info("{}{}", pjpMethodInfo, this.getRequestInfo(pjp));
         long beginTime = System.currentTimeMillis();
         Object retObj = pjp.proceed();
         long costMs = System.currentTimeMillis() - beginTime;
-        log.info("{} | {} | cost:{}ms", pjpMethodInfo, objectMapper.writeValueAsString(retObj), costMs);
+        log.info("{} | {} | 耗时：{}ms", pjpMethodInfo, objectMapper.writeValueAsString(retObj), costMs);
         return retObj;
     }
 
@@ -68,27 +66,29 @@ public class ControllerLogAspect {
         StrBuilder sb = StrBuilder.create();
         try {
             HttpServletRequest request = ((ServletRequestAttributes) Objects.requireNonNull(RequestContextHolder.getRequestAttributes())).getRequest();
-            String xGatewayUid = request.getHeader(CommonConstants.GATEWAY_UID_HEADER);
             String method = request.getMethod();
-            sb.append("method:[").append(method).append("] | ");
-            sb.append("requestPath:[").append(request.getRequestURI()).append("] | ");
-            sb.append("X-GATEWAY-UID:[").append(Objects.isNull(xGatewayUid) ? "" : xGatewayUid).append("] | ");
+            sb.append(" | method:[").append(method).append("]");
+            sb.append(" | requestPath:[").append(request.getRequestURI()).append("]");
             if (Method.POST.toString().equalsIgnoreCase(method)) {
                 Object[] args = point.getArgs();
                 if (args.length > 0) {
-                    sb.append("args:");
+                    sb.append(" | args:[");
                     for (Object arg : args) {
                         if (arg instanceof Serializable && !(arg instanceof MultipartFile)) {
                             sb.append(objectMapper.writeValueAsString(arg)).append(",");
                         }
                     }
+                    sb.append("]");
                 }
                 Map<String, String[]> parameterMap = request.getParameterMap();
                 if (parameterMap != null && parameterMap.size() > 0) {
-                    sb.append("param:").append(objectMapper.writeValueAsString(parameterMap));
+                    sb.append(" | param:[").append(objectMapper.writeValueAsString(parameterMap)).append("]");
                 }
             } else {
-                sb.append("queryString:").append(request.getQueryString());
+                String queryString = request.getQueryString();
+                if (StrUtil.isNotBlank(queryString)) {
+                    sb.append(" | queryString:[").append(request.getQueryString()).append("]");
+                }
             }
         } catch (Exception e) {
             sb.append("Exception:").append(e.getMessage());
