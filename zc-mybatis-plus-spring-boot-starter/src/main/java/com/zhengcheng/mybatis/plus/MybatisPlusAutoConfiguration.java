f794467e88a78ccb86506f446616d727d1556eac
quansheng1.zhang <quansheng1.zhang@zhangmen.com>
2021-07-15 17:00:21 +0800

    数据库表公共字段：id、is_deleted、create_user_id、create_time、update_user_id、update_time

--- zc-mybatis-plus-spring-boot-starter/src/main/java/com/zhengcheng/mybatis/plus/MybatisPlusAutoConfiguration.java
+++ zc-mybatis-plus-spring-boot-starter/src/main/java/com/zhengcheng/mybatis/plus/MybatisPlusAutoConfiguration.java
@@ -28,7 +28,7 @@ public class MybatisPlusAutoConfiguration {
     public MybatisPlusAutoConfiguration() {
         log.info("------ mybatis-plus 自动配置  ---------------------------------------");
         log.info("------ 事务管理注解 @EnableTransactionManagement 配置成功 ");
-        log.info("------ 数据库表公共字段：id、create_time、update_time、is_deleted ");
+        log.info("------ 数据库表公共字段：id、is_deleted、create_user_id、create_time、update_user_id、update_time ");
         log.info("------ MapperScan com.zhengcheng.**.mapper* ");
         log.info("-----------------------------------------------------------------");
     }
