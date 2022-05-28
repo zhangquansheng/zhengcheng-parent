46080dbb9d096ee6d9928b14616e8f94fe73000f
yl-zhangquansheng <zhangquansheng@za-doctor.com>
2022-05-28 15:33:01 +0800

    Spring Cloud Gateway 自动配置

---
+++ zc-gateway-spring-boot-starter/src/main/java/com/zhengcheng/gw/GatewayAutoConfiguration.java
@@ -0,0 +1,14 @@
+package com.zhengcheng.gw;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Spring Cloud Gateway 自动配置
+ *
+ * @author quansheng1.zhang
+ * @since 2022/05/28 13:01
+ */
+@Configuration
+public class GatewayAutoConfiguration {
+
+}
