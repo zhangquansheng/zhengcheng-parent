455f56cc129187b1118de0e802d369ec44af1ccf
yl-zhangquansheng <zhangquansheng@za-doctor.com>
2022-06-13 23:28:18 +0800

    SwaggerAutoConfiguration

--- zc-swagger-spring-boot-starter/src/main/java/com/zhengcheng/swagger/SwaggerAutoConfiguration.java
+++ zc-swagger-spring-boot-starter/src/main/java/com/zhengcheng/swagger/SwaggerAutoConfiguration.java
@@ -1,13 +1,18 @@
 package com.zhengcheng.swagger;
 
 import com.zhengcheng.swagger.properties.SwaggerProperties;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.core.annotation.Order;
+import org.springframework.util.ReflectionUtils;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfoHandlerMapping;
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -16,7 +21,13 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.spring.web.plugins.WebFluxRequestHandlerProvider;
+import springfox.documentation.spring.web.plugins.WebMvcRequestHandlerProvider;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.lang.reflect.Field;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * SwaggerAutoConfiguration
@@ -36,14 +47,21 @@ public class SwaggerAutoConfiguration {
         log.info("------ Knife4j-3.0.3 自动配置  ---------------------------------------");
     }
 
-    @Bean(value = "knife4jRestApi")
-    @Order(value = 1)
-    public Docket createRestApi(SwaggerProperties swaggerProperties) {
-        return new Docket(DocumentationType.OAS_30)
+    /**
+     * swagger2的配置文件，这里可以配置swagger2的一些基本的内容，比如扫描的包等等
+     *
+     * @return Docket
+     */
+    @Bean(value = "defaultApi2")
+    public Docket defaultApi2(SwaggerProperties swaggerProperties) {
+        return new Docket(DocumentationType.SWAGGER_2)
                 .groupName(swaggerProperties.getGroupName())
                 .apiInfo(apiInfo(swaggerProperties))
                 .select()
                 .apis(RequestHandlerSelectors.basePackage(swaggerProperties.getBasePackage()))
+                //加了ApiOperation注解的类，才生成接口文档
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
                 .paths(PathSelectors.any())
                 .build()
                 .enable(swaggerProperties.getEnable() != null && swaggerProperties.getEnable());
@@ -59,6 +77,44 @@ public class SwaggerAutoConfiguration {
                 .license(swaggerProperties.getLicense())
                 .licenseUrl(swaggerProperties.getLicenseUrl())
                 .build();
+    }
+
+    /**
+     * 解决springboot2.6 和springfox不兼容问题
+     *
+     * @return BeanPostProcessor
+     */
+    @Bean
+    public static BeanPostProcessor springfoxHandlerProviderBeanPostProcessor() {
+        return new BeanPostProcessor() {
+
+            @Override
+            public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+                if (bean instanceof WebMvcRequestHandlerProvider || bean instanceof WebFluxRequestHandlerProvider) {
+                    customizeSpringfoxHandlerMappings(getHandlerMappings(bean));
+                }
+                return bean;
+            }
+
+            private <T extends RequestMappingInfoHandlerMapping> void customizeSpringfoxHandlerMappings(List<T> mappings) {
+                List<T> copy = mappings.stream()
+                        .filter(mapping -> mapping.getPatternParser() == null)
+                        .collect(Collectors.toList());
+                mappings.clear();
+                mappings.addAll(copy);
+            }
+
+            @SuppressWarnings("unchecked")
+            private List<RequestMappingInfoHandlerMapping> getHandlerMappings(Object bean) {
+                try {
+                    Field field = ReflectionUtils.findField(bean.getClass(), "handlerMappings");
+                    field.setAccessible(true);
+                    return (List<RequestMappingInfoHandlerMapping>) field.get(bean);
+                } catch (IllegalArgumentException | IllegalAccessException e) {
+                    throw new IllegalStateException(e);
+                }
+            }
+        };
     }
 
 }
