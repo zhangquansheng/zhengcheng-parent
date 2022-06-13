6a67673aa73f0a7411d46cf0881a9c609998bfec
yl-zhangquansheng <zhangquansheng@za-doctor.com>
2022-06-13 14:53:39 +0800

    https://github.com/springfox/springfox/issues/3462

--- zc-swagger-spring-boot-starter/src/main/java/com/zhengcheng/swagger/SwaggerAutoConfiguration.java
+++ zc-swagger-spring-boot-starter/src/main/java/com/zhengcheng/swagger/SwaggerAutoConfiguration.java
@@ -1,12 +1,24 @@
 package com.zhengcheng.swagger;
 
-import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
 import com.zhengcheng.swagger.properties.SwaggerProperties;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.actuate.autoconfigure.endpoint.web.CorsEndpointProperties;
+import org.springframework.boot.actuate.autoconfigure.endpoint.web.WebEndpointProperties;
+import org.springframework.boot.actuate.autoconfigure.web.server.ManagementPortType;
+import org.springframework.boot.actuate.endpoint.ExposableEndpoint;
+import org.springframework.boot.actuate.endpoint.web.*;
+import org.springframework.boot.actuate.endpoint.web.annotation.ControllerEndpointsSupplier;
+import org.springframework.boot.actuate.endpoint.web.annotation.ServletEndpointsSupplier;
+import org.springframework.boot.actuate.endpoint.web.servlet.WebMvcEndpointHandlerMapping;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.core.annotation.Order;
+import org.springframework.core.env.Environment;
+import org.springframework.util.StringUtils;
+import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -14,6 +26,11 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 /**
  * SwaggerAutoConfiguration
@@ -24,7 +41,8 @@ import springfox.documentation.spring.web.plugins.Docket;
 @Slf4j
 @ConditionalOnProperty(prefix = "spring.swagger", name = "enable", havingValue = "true")
 @Configuration
-@EnableKnife4j
+@EnableSwagger2
+@Import(BeanValidatorPluginsConfiguration.class)
 @EnableConfigurationProperties({SwaggerProperties.class})
 public class SwaggerAutoConfiguration {
 
@@ -32,7 +50,8 @@ public class SwaggerAutoConfiguration {
         log.info("------ Knife4j-3.0.3 自动配置  ---------------------------------------");
     }
 
-    @Bean
+    @Bean(value = "knife4jRestApi")
+    @Order(value = 1)
     public Docket createRestApi(SwaggerProperties swaggerProperties) {
         return new Docket(DocumentationType.OAS_30)
                 .groupName(swaggerProperties.getGroupName())
@@ -41,7 +60,7 @@ public class SwaggerAutoConfiguration {
                 .apis(RequestHandlerSelectors.basePackage(swaggerProperties.getBasePackage()))
                 .paths(PathSelectors.any())
                 .build()
-                .enable(swaggerProperties.getEnable() == null ? false : swaggerProperties.getEnable());
+                .enable(swaggerProperties.getEnable() != null && swaggerProperties.getEnable());
     }
 
     private ApiInfo apiInfo(SwaggerProperties swaggerProperties) {
@@ -54,6 +73,25 @@ public class SwaggerAutoConfiguration {
                 .license(swaggerProperties.getLicense())
                 .licenseUrl(swaggerProperties.getLicenseUrl())
                 .build();
+    }
+
+    // https://github.com/springfox/springfox/issues/3462
+    @Bean
+    public WebMvcEndpointHandlerMapping webEndpointServletHandlerMapping(WebEndpointsSupplier webEndpointsSupplier, ServletEndpointsSupplier servletEndpointsSupplier, ControllerEndpointsSupplier controllerEndpointsSupplier, EndpointMediaTypes endpointMediaTypes, CorsEndpointProperties corsProperties, WebEndpointProperties webEndpointProperties, Environment environment) {
+        List<ExposableEndpoint<?>> allEndpoints = new ArrayList();
+        Collection<ExposableWebEndpoint> webEndpoints = webEndpointsSupplier.getEndpoints();
+        allEndpoints.addAll(webEndpoints);
+        allEndpoints.addAll(servletEndpointsSupplier.getEndpoints());
+        allEndpoints.addAll(controllerEndpointsSupplier.getEndpoints());
+        String basePath = webEndpointProperties.getBasePath();
+        EndpointMapping endpointMapping = new EndpointMapping(basePath);
+        boolean shouldRegisterLinksMapping = this.shouldRegisterLinksMapping(webEndpointProperties, environment, basePath);
+        return new WebMvcEndpointHandlerMapping(endpointMapping, webEndpoints, endpointMediaTypes, corsProperties.toCorsConfiguration(), new EndpointLinksResolver(allEndpoints, basePath), shouldRegisterLinksMapping, null);
+    }
+
+
+    private boolean shouldRegisterLinksMapping(WebEndpointProperties webEndpointProperties, Environment environment, String basePath) {
+        return webEndpointProperties.getDiscovery().isEnabled() && (StringUtils.hasText(basePath) || ManagementPortType.get(environment).equals(ManagementPortType.DIFFERENT));
     }
 
 }
