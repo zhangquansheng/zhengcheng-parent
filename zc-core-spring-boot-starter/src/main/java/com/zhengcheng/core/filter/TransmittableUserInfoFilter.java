02fe0801679f6ba47a825018747bf2044291c804
yl-zhangquansheng <zhangquansheng@za-doctor.com>
2022-07-04 18:22:08 +0800

    TransmittableUserInfoFilter extends GenericFilterBean

--- zc-core-spring-boot-starter/src/main/java/com/zhengcheng/core/filter/TransmittableUserInfoFilter.java
+++ zc-core-spring-boot-starter/src/main/java/com/zhengcheng/core/filter/TransmittableUserInfoFilter.java
@@ -6,8 +6,12 @@ import com.zhengcheng.common.dto.UserInfo;
 import com.zhengcheng.common.holder.ZcUserContextHolder;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.filter.GenericFilterBean;
 
-import javax.servlet.*;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
@@ -19,30 +23,31 @@ import java.io.IOException;
  */
 @Slf4j
 @RequiredArgsConstructor
-public class TransmittableUserInfoFilter implements Filter {
+public class TransmittableUserInfoFilter extends GenericFilterBean {
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
-        String uid = request.getHeader(CommonConstants.HEADER_GATEWAY_UID);
-        String userNo = request.getHeader(CommonConstants.HEADER_GATEWAY_USER_NO);
-        String username = request.getHeader(CommonConstants.HEADER_GATEWAY_USERNAME);
-        String clientIp = request.getHeader(CommonConstants.HEADER_CLIENT_IP);
 
-        UserInfo userInfo = new UserInfo();
-        userInfo.setId(StrUtil.isBlank(uid) ? null : Long.valueOf(uid));
-        userInfo.setUserNo(userNo);
-        userInfo.setUsername(username);
-        userInfo.setClientIp(clientIp);
+        try {
+            String uid = request.getHeader(CommonConstants.HEADER_GATEWAY_UID);
+            String userNo = request.getHeader(CommonConstants.HEADER_GATEWAY_USER_NO);
+            String username = request.getHeader(CommonConstants.HEADER_GATEWAY_USERNAME);
+            String clientIp = request.getHeader(CommonConstants.HEADER_CLIENT_IP);
 
-        //将UserInfo放入上下文中
-        ZcUserContextHolder.setUserInfo(userInfo);
+            UserInfo userInfo = new UserInfo();
+            userInfo.setId(StrUtil.isBlank(uid) ? null : Long.valueOf(uid));
+            userInfo.setUserNo(userNo);
+            userInfo.setUsername(username);
+            userInfo.setClientIp(clientIp);
 
-        filterChain.doFilter(servletRequest, servletResponse);
+            //将UserInfo放入上下文中
+            ZcUserContextHolder.setUserInfo(userInfo);
+
+            filterChain.doFilter(servletRequest, servletResponse);
+        } finally {
+            ZcUserContextHolder.removeUserInfo();
+        }
     }
 
-    @Override
-    public void destroy() {
-        ZcUserContextHolder.removeUserInfo();
-    }
 }
