4c5bdd362026c16cc559895b58911b6938f9d1a2
zhangquansheng <952547584@qq.com>
2022-08-17 11:31:57 +0800

    v6.1.0-SNAPSHOT @ConditionalOnBean

--- zc-cache-spring-boot-starter/src/main/java/com/zhengcheng/cache/RedissonAutoConfiguration.java
+++ zc-cache-spring-boot-starter/src/main/java/com/zhengcheng/cache/RedissonAutoConfiguration.java
@@ -11,6 +11,7 @@ import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
 import org.redisson.config.SentinelServersConfig;
 import org.redisson.config.SingleServerConfig;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -79,7 +80,7 @@ public class RedissonAutoConfiguration {
 
 
     @Bean
-    @ConditionalOnClass(RedissonClient.class)
+    @ConditionalOnBean(RedissonClient.class)
     public IdempotentAspect idempotentAspect() {
         return new IdempotentAspect();
     }
