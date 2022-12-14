483d72ea1b2bcb3bb20a45be40cd3dc88718eeb1
zhangquansheng <952547584@qq.com>
2022-12-14 20:50:31 +0800

    6.1.0-SNAPSHOT

--- zc-rocket-mq-spring-boot-starter/src/main/java/com/zhengcheng/rocket/mq/aspect/RocketMQDedupAspect.java
+++ zc-rocket-mq-spring-boot-starter/src/main/java/com/zhengcheng/rocket/mq/aspect/RocketMQDedupAspect.java
@@ -1,7 +1,7 @@
 package com.zhengcheng.rocket.mq.aspect;
 
 import cn.hutool.core.util.StrUtil;
-import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson2.JSON;
 import com.aliyun.openservices.ons.api.Action;
 import com.aliyun.openservices.ons.api.ConsumeContext;
 import com.aliyun.openservices.ons.api.Message;
