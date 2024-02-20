25d00d87c0da0b620389172474ee0cdecb685796
zhangquansheng <952547584@qq.com>
2024-02-20 15:30:53 +0800

    [v6.1.1] 整理 `zhengcheng-parent` 项目的系统模块，leaf

--- zhengcheng-starter/zhengcheng-leaf-spring-boot-starter/src/main/java/com/zhengcheng/leaf/exception/LeafServerException.java
+++ zhengcheng-starter/zhengcheng-leaf-spring-boot-starter/src/main/java/com/zhengcheng/leaf/exception/LeafServerException.java
@@ -1,9 +1,5 @@
 package com.zhengcheng.leaf.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(code= HttpStatus.INTERNAL_SERVER_ERROR)
 public class LeafServerException extends RuntimeException {
     public LeafServerException(String msg) {
         super(msg);
