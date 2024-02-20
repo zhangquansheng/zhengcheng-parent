c501949ea9c3eac9ab9aa221008193d73c114b25
zhangquansheng <952547584@qq.com>
2024-02-20 14:55:25 +0800

    [v6.1.1] 整理 `zhengcheng-parent` 项目的系统模块，基础依赖包

---
+++ zhengcheng-starter/zhengcheng-knife4j-spring-boot-starter/src/main/java/com/zhengcheng/swagger/properties/SwaggerProperties.java
@@ -0,0 +1,29 @@
+package com.zhengcheng.swagger.properties;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * SwaggerProperties
+ *
+ * @author :    quansheng.zhang
+ * @date :    2019/2/2 15:52
+ */
+@Data
+@ConfigurationProperties(
+        prefix = "spring.swagger"
+)
+public class SwaggerProperties {
+
+    private String groupName;
+    private String title;
+    private String description;
+    private String termsOfServiceUrl;
+    private String contactName;
+    private String contactUrl;
+    private String contactEmail;
+    private String license;
+    private String licenseUrl;
+    private String version;
+    private String basePackage;
+}
