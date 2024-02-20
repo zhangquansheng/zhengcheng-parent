c24d809cda7f9f6f2be31faae4dbf0fb6057dd0a
zhangquansheng <952547584@qq.com>
2024-02-20 15:34:54 +0800

    [v6.1.1] 整理 `zhengcheng-parent` 项目的系统模块，redis

---
+++ zhengcheng-starter/zhengcheng-cache-spring-boot-starter/src/main/java/com/zhengcheng/cache/RedisAutoConfiguration.java
@@ -0,0 +1,100 @@
+package com.zhengcheng.cache;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.zhengcheng.cache.ratelimit.aspect.RateLimiterAspect;
+import com.zhengcheng.cache.ratelimit.handler.DefaultLimitKeyHandler;
+import com.zhengcheng.cache.ratelimit.handler.LimitKeyHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.scripting.support.ResourceScriptSource;
+
+/**
+ * redis 配置类
+ * https://docs.spring.io/spring-data/data-redis/docs/current/reference/html/#redis
+ *
+ * @author :    quansheng.zhang
+ * @date :    2019/8/12 22:41
+ */
+@Slf4j
+@EnableCaching
+public class RedisAutoConfiguration extends CachingConfigurerSupport {
+
+    public RedisAutoConfiguration() {
+        log.info("------ redis 缓存管理 自动配置  ----------------------------------");
+        log.info("------ @EnableCaching 启动缓存管理 ");
+        log.info("-----------------------------------------------------------------");
+    }
+
+    /**
+     * RedisTemplate配置
+     *
+     * @param factory RedisConnectionFactory
+     */
+    @Primary
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(factory);
+
+        //创建 Jackson2JsonRedisSerializer 序列方式，对象类型使用 Object 类型，
+        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        objectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        //设置一下 jackJson 的 ObjectMapper 对象参数
+        jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
+
+        // key 序列化规则
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        // value 序列化规则
+        redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
+        // hash key 序列化规则
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        // hash value 序列化规则
+        redisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer);
+        //属性设置后操作
+        redisTemplate.afterPropertiesSet();
+        return redisTemplate;
+    }
+
+    @Primary
+    @Bean
+    public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory connectionFactory) {
+        return new StringRedisTemplate(connectionFactory);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(value = LimitKeyHandler.class)
+    public LimitKeyHandler stringRandomGenerator() {
+        return new DefaultLimitKeyHandler();
+    }
+
+    @Bean
+    public DefaultRedisScript<Boolean> redisScript() {
+        DefaultRedisScript<Boolean> redisScript = new DefaultRedisScript<>();
+        redisScript.setScriptSource(new ResourceScriptSource(new ClassPathResource("script/rate_limit.lua")));
+        redisScript.setResultType(Boolean.class);
+        return redisScript;
+    }
+
+    @Bean
+    @ConditionalOnBean({LimitKeyHandler.class, DefaultRedisScript.class, StringRedisTemplate.class})
+    public RateLimiterAspect rateLimiterAspect() {
+        return new RateLimiterAspect();
+    }
+
+}
---
+++ zhengcheng-starter/zhengcheng-cache-spring-boot-starter/src/main/java/com/zhengcheng/cache/ratelimit/annotation/RateLimiter.java
@@ -0,0 +1,51 @@
+package com.zhengcheng.cache.ratelimit.annotation;
+
+import com.zhengcheng.cache.ratelimit.enums.LimitType;
+
+import java.lang.annotation.*;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 限流器
+ *
+ * @author :    zhangquansheng
+ * @date :    2020/5/12 16:11
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@Documented
+public @interface RateLimiter {
+
+    /**
+     * 允许访问的最大次数
+     */
+    int count() default 10;
+
+    /**
+     * 时间段，默认值 1 秒
+     */
+    long time() default 1000;
+
+    /**
+     * 时间段的时间单位，默认 秒
+     */
+    TimeUnit timeUnit() default TimeUnit.MILLISECONDS;
+
+    /**
+     * 限流类型，可组合
+     */
+    LimitType[] limitType() default LimitType.METHOD;
+
+    /**
+     * 限流的keys，在 LimitType.ARGS 的情况下生效
+     * spel表达式，支持多项拼接
+     */
+    String[] keys() default {};
+
+    /**
+     * 限流keys的分隔符，在 LimitType.ARGS 的情况下生效
+     */
+    String split() default "-";
+
+    String message() default "RateLimitInvocationException";
+}
