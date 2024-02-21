ed193eae5e7f3e6f6c561ea403b4ac98c53347fb
zhangquansheng <952547584@qq.com>
2024-02-21 13:34:02 +0800

    [v6.1.1] 整理 `zhengcheng-parent` 项目的系统模块 微服务 ums

--- zhengcheng-micro/ums/src/main/java/com/zhengcheng/ums/controller/facade/internal/OauthFacadeImpl.java
+++ zhengcheng-micro/ums/src/main/java/com/zhengcheng/ums/controller/facade/internal/OauthFacadeImpl.java
@@ -25,11 +25,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
 import java.time.LocalDateTime;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
@@ -39,11 +36,10 @@ import cn.dev33.satoken.stp.SaTokenInfo;
 import cn.dev33.satoken.stp.StpUtil;
 import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.net.NetUtil;
+import cn.hutool.core.text.CharSequenceUtil;
 import cn.hutool.core.util.RandomUtil;
-import cn.hutool.core.util.StrUtil;
 import cn.hutool.crypto.SecureUtil;
 import cn.hutool.extra.servlet.ServletUtil;
-import cn.hutool.http.HttpUtil;
 import lombok.extern.slf4j.Slf4j;
 
 import static cn.hutool.core.date.DatePattern.PURE_DATE_FORMAT;
@@ -101,7 +97,7 @@ public class OauthFacadeImpl implements OauthFacade {
         String key = getMobileLoginKey(userMobileLoginCommand.getMobile());
         String code = redisTemplate.opsForValue().get(key);
         // 前端对 code md5加密了
-        if (StrUtil.isBlank(code) || !userMobileLoginCommand.getCode().equals(SecureUtil.md5(code))) {
+        if (CharSequenceUtil.isBlank(code) || !userMobileLoginCommand.getCode().equals(SecureUtil.md5(code))) {
             throw new BizException("验证码不正确");
         }
 
@@ -132,13 +128,13 @@ public class OauthFacadeImpl implements OauthFacade {
         // 获取当天发送短信验证码的次数
         String sendNumKey = getMobileSendNumKey(sendSmsCommand.getMobile());
         String sendNumStr = redisTemplate.opsForValue().get(sendNumKey);
-        if (StrUtil.isNotBlank(sendNumStr) && Objects.nonNull(sendNumStr) && Integer.parseInt(sendNumStr) >= 5) {
+        if (CharSequenceUtil.isNotBlank(sendNumStr) && Objects.nonNull(sendNumStr) && Integer.parseInt(sendNumStr) >= 5) {
             throw new BizException("验证码请求今日已达上线");
         }
 
         String key = getMobileLoginKey(sendSmsCommand.getMobile());
         String existingCode = redisTemplate.opsForValue().get(key);
-        if (StrUtil.isNotBlank(existingCode)) {
+        if (CharSequenceUtil.isNotBlank(existingCode)) {
             throw new BizException("获取验证码请求太频繁，请稍后重试！");
         }
 
@@ -147,19 +143,8 @@ public class OauthFacadeImpl implements OauthFacade {
 
         log.info("发送手机号{}，验证码: {}", sendSmsCommand.getMobile(), code);
 
-        Map<String, Object> param = new HashMap<>();
-        param.put("uid", "175739");
-        param.put("pwd", SecureUtil.md5("9wd02x"));
-        param.put("mobile", sendSmsCommand.getMobile());
-        param.put("srcphone", "75739");
-        param.put("msg", URLEncoder.encode(StrUtil.format("【东证期货】验证码{}，感谢您参与本次竞答活动。如非本人操作，请忽略此短信。", code), "UTF-8"));
-        String url = "http://bst.8315.cn:9892/cmppweb/sendsms";
-        String res = HttpUtil.post(url, param);
-
         int sendNum = Objects.nonNull(sendNumStr) ? Integer.parseInt(sendNumStr) + 1 : 1;
         redisTemplate.opsForValue().set(sendNumKey, sendNum + "", 1, TimeUnit.DAYS);
-
-        log.info("发送验证码的接口返回结果: {}", res);
     }
 
     /**
@@ -182,7 +167,7 @@ public class OauthFacadeImpl implements OauthFacade {
         userService.updateById(user);
 
         String userActivityKeyPrefix = "zc:user:act:bm:";
-        String dayKey = StrUtil.format("{}{}", userActivityKeyPrefix, DateUtil.format(new Date(), PURE_DATE_FORMAT));
+        String dayKey = CharSequenceUtil.format("{}{}", userActivityKeyPrefix, DateUtil.format(new Date(), PURE_DATE_FORMAT));
         // 每天按日期生成一个位图
         stringRedisTemplate.opsForValue().setBit(dayKey, user.getId(), Boolean.TRUE);
 
@@ -194,9 +179,9 @@ public class OauthFacadeImpl implements OauthFacade {
         int maxDayNum = 30;
         byte[][] bytes = new byte[maxDayNum][];
         for (int offset = 0; offset < maxDayNum; offset++) {
-            bytes[offset] = StrUtil.format("{}{}", userActivityKeyPrefix, DateUtil.format(DateUtil.offsetDay(new Date(), -offset), PURE_DATE_FORMAT)).getBytes();
+            bytes[offset] = CharSequenceUtil.format("{}{}", userActivityKeyPrefix, DateUtil.format(DateUtil.offsetDay(new Date(), -offset), PURE_DATE_FORMAT)).getBytes();
         }
-        String monthKey = StrUtil.format("{}{}", userActivityKeyPrefix, 30);
+        String monthKey = CharSequenceUtil.format("{}{}", userActivityKeyPrefix, 30);
         stringRedisTemplate.execute((RedisCallback<Long>) con -> con.bitOp(RedisStringCommands.BitOperation.OR, monthKey.getBytes(), bytes));
         Long monthActivityNum = stringRedisTemplate.execute((RedisCallback<Long>) con -> con.bitCount(monthKey.getBytes()));
         log.info("用户月活：{}", monthActivityNum);
