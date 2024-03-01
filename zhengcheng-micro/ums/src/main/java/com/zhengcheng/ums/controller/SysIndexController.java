ba6a892b695982a9c5dcda6fbc4389622d4392fc
zhangquansheng <952547584@qq.com>
2024-03-01 11:03:52 +0800

    [v6.1.1] 整理 `zhengcheng-parent` 项目的系统模块 微服务 ums

---
+++ zhengcheng-micro/ums/src/main/java/com/zhengcheng/ums/controller/SysIndexController.java
@@ -0,0 +1,51 @@
+package com.zhengcheng.ums.controller;
+
+import com.oddfar.campus.common.config.CampusConfig;
+import com.oddfar.campus.common.domain.R;
+import com.oddfar.campus.common.utils.StringUtils;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+
+/**
+ * 首页
+ *
+ * @author oddfar
+ */
+@RestController
+public class SysIndexController {
+    /**
+     * 系统基础配置
+     */
+    @Autowired
+    private CampusConfig campusConfig;
+
+    /**
+     * 访问首页，提示语
+     */
+    @RequestMapping("/")
+    public String index() {
+        return StringUtils.format("欢迎使用{}后台管理框架，当前版本：v{}，请通过前端地址访问。", campusConfig.getName(), campusConfig.getVersion());
+    }
+
+    @Value("${campus.version}")
+    private String version;
+
+    @Value("${campus.frameworkVersion}")
+    private String frameworkVersion;
+
+    /**
+     * 版本情况
+     */
+    @RequestMapping("/version")
+    public R version() {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("version", version);
+        map.put("frameworkVersion", frameworkVersion);
+        return R.ok(map);
+    }
+}
---
+++ zhengcheng-micro/ums/src/main/java/com/zhengcheng/ums/mapper/SysConfigMapper.java
@@ -0,0 +1,45 @@
+package com.zhengcheng.ums.mapper;
+
+
+import com.zhengcheng.ums.domain.entity.SysConfigEntity;
+
+import cn.hutool.core.util.ObjectUtil;
+
+public interface SysConfigMapper extends BaseMapper<SysConfigEntity> {
+
+    default PageResult<SysConfigEntity> selectPage(SysConfigEntity config) {
+
+        return selectPage(new LambdaQueryWrapperX<SysConfigEntity>()
+                .likeIfPresent(SysConfigEntity::getConfigName, config.getConfigName())
+                .likeIfPresent(SysConfigEntity::getConfigKey, config.getConfigKey())
+                .eqIfPresent(SysConfigEntity::getGroupCode, config.getGroupCode())
+                .betweenIfPresent(SysConfigEntity::getCreateTime, config.getParams())
+        );
+
+    }
+
+
+    /**
+     * 查询参数配置信息
+     *
+     * @param config 参数配置信息
+     * @return 参数配置信息
+     */
+    default SysConfigEntity selectConfig(SysConfigEntity config) {
+
+        return selectOne(new LambdaQueryWrapperX<SysConfigEntity>()
+                .eq(ObjectUtil.isNotEmpty(config.getConfigId()), SysConfigEntity::getConfigId, config.getConfigId())
+                .eq(ObjectUtil.isNotEmpty(config.getConfigName()), SysConfigEntity::getConfigName, config.getConfigName())
+                .eq(ObjectUtil.isNotEmpty(config.getConfigKey()), SysConfigEntity::getConfigKey, config.getConfigKey()));
+    }
+
+    /**
+     * 校验参数键名是否唯一
+     */
+    default SysConfigEntity checkConfigKeyUnique(SysConfigEntity config) {
+        return selectOne(new LambdaQueryWrapperX<SysConfigEntity>()
+                .eq(SysConfigEntity::getConfigKey, config.getConfigKey())
+        );
+    }
+
+}
