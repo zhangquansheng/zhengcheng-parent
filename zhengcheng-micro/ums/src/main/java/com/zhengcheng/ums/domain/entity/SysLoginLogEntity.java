66498c4c86e6ec2cfa98263541857f80c69913cd
zhangquansheng <952547584@qq.com>
2024-03-04 17:23:36 +0800

    [v6.1.1] 整理 `zhengcheng-parent` 项目的系统模块 微服务 ums 分页

--- zhengcheng-micro/ums/src/main/java/com/zhengcheng/ums/domain/entity/SysLoginLogEntity.java
+++ zhengcheng-micro/ums/src/main/java/com/zhengcheng/ums/domain/entity/SysLoginLogEntity.java
@@ -4,15 +4,9 @@ import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import java.util.Date;
-import java.util.HashMap;
 import java.util.Map;
-
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
 
 import lombok.Data;
 
@@ -78,30 +72,7 @@ public class SysLoginLogEntity {
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date loginTime;
 
-
-    private static final Integer PAGE_NUM = 1;
-    private static final Integer PAGE_SIZE = 10;
-
     @TableField(exist = false)
     private Map<String, Object> params;
 
-    @NotNull(message = "页码不能为空")
-    @Min(value = 1, message = "页码最小值为 1")
-    @TableField(exist = false)
-    @JsonIgnore
-    private Integer pageNum = PAGE_NUM;
-
-    @NotNull(message = "每页条数不能为空")
-    @Min(value = 1, message = "每页条数最小值为 1")
-    @Max(value = 100, message = "每页条数最大值为 100")
-    @TableField(exist = false)
-    @JsonIgnore
-    private Integer pageSize = PAGE_SIZE;
-
-    public Map<String, Object> getParams() {
-        if (params == null) {
-            params = new HashMap<>();
-        }
-        return params;
-    }
 }
--- zhengcheng-starter/zhengcheng-mp-spring-boot-starter/src/main/java/com/zhengcheng/mybatis/plus/model/BaseEntity.java
+++ zhengcheng-starter/zhengcheng-mp-spring-boot-starter/src/main/java/com/zhengcheng/mybatis/plus/model/BaseEntity.java
@@ -1,15 +1,11 @@
 package com.zhengcheng.mybatis.plus.model;
 
 import com.baomidou.mybatisplus.annotation.FieldFill;
-import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
-import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableLogic;
 import com.baomidou.mybatisplus.extension.activerecord.Model;
 
 import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.Map;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -28,8 +24,8 @@ public class BaseEntity<T extends Model<?>> extends Model<T> {
     /**
      * 主键ID
      */
-    @TableId(type = IdType.AUTO)
-    private Long id;
+//    @TableId(type = IdType.AUTO)
+//    private Long id;
     /**
      * 创建时间
      */
