fb3acd3f0c3b15b390f341aeeeeadf6c68f7dc6d
zhangquansheng <952547584@qq.com>
2024-03-08 15:47:22 +0800

    [v6.1.1] 整理 `zhengcheng-parent` 项目的系统模块 ums

--- zhengcheng-plugin/zhengcheng-sa-token-core/src/main/java/com/zhengcheng/satoken/utils/SaTokenUtil.java
+++ zhengcheng-plugin/zhengcheng-sa-token-core/src/main/java/com/zhengcheng/satoken/utils/SaTokenUtil.java
@@ -2,8 +2,13 @@ package com.zhengcheng.satoken.utils;
 
 import com.zhengcheng.satoken.domain.LoginUser;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import cn.dev33.satoken.session.SaSession;
+import cn.dev33.satoken.session.SaSessionCustomUtil;
 import cn.dev33.satoken.stp.StpUtil;
+import cn.hutool.core.collection.CollUtil;
 
 /**
  * 基于 sa-token 的用户权限认证工具类
@@ -12,6 +17,9 @@ import cn.dev33.satoken.stp.StpUtil;
  * @since 2024/3/8 15:33
  */
 public class SaTokenUtil {
+
+    private SaTokenUtil() {
+    }
 
     /**
      * 用户ID
@@ -47,10 +55,51 @@ public class SaTokenUtil {
 
     /**
      * 是否登录，true登录
-     *
-     * @return
      */
     public static boolean isLogin() {
         return StpUtil.isLogin();
+    }
+
+    private static final String ROLE_SESSION_ID_PREFIX = "zhengcheng-user-role-";
+
+    /**
+     * 给角色设置权限
+     *
+     * @param roleId 角色ID
+     * @param perms  权限
+     */
+    public static void setPermsByRoleId(Object roleId, List<String> perms) {
+        if (CollUtil.isEmpty(perms)) {
+            return;
+        }
+        SaSession roleSession = SaSessionCustomUtil.getSessionById(ROLE_SESSION_ID_PREFIX + roleId);
+        roleSession.set(SaSession.PERMISSION_LIST, perms);
+    }
+
+    /**
+     * 返回角色ID权限集合
+     *
+     * @param roleIds 角色ID
+     * @return 权限集合
+     */
+    public static List<String> getPermsByRoleIds(List<Object> roleIds) {
+        // 现在假设如下业务场景：我们系统中有十万个账号属于同一个角色，当我们变动这个角色的权限时，难道我们要同时清除这十万个账号的缓存信息吗？ 这显然是一个不合理的操作，同一时间缓存大量清除容易引起Redis的缓存雪崩
+
+        // 1. 声明权限码集合
+        List<String> permissionList = new ArrayList<>();
+        if (CollUtil.isEmpty(roleIds)) {
+            return permissionList;
+        }
+        // 2. 遍历角色列表，查询拥有的权限码
+        for (Object roleId : roleIds) {
+            SaSession roleSession = SaSessionCustomUtil.getSessionById(ROLE_SESSION_ID_PREFIX + roleId);
+            List<String> perms = (List<String>) roleSession.get(SaSession.PERMISSION_LIST);
+            if (CollUtil.isNotEmpty(perms)) {
+                permissionList.addAll(perms);
+            }
+        }
+
+        // 3. 返回权限码集合
+        return permissionList;
     }
 }
